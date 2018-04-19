5ca5aafd06eb90f748655eabc24a195c2f6b2f79
Andrew Jarrett <ahrjarrett@gmail.com>
2018-04-18 21:44:39 -0600

    add todos to localStorage -- duplicating 1st todo on refresh

--- todo/src/todoReducer.js
+++ todo/src/todoReducer.js
@@ -1,7 +1,14 @@
 import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from './todoAction';
 import { v4 } from 'uuid';
 
-const todoReducer = (state = [], action) => {
+const storage = JSON.parse(localStorage.getItem('todos')) || [];
+
+const addToLocalStorage = newTodo => {
+  storage.push(newTodo)
+  localStorage.setItem('todos', JSON.stringify(storage))
+}
+
+const todoReducer = (state = storage, action) => {
   switch (action.type) {
   case ADD_TODO:
     const newTodo = {
@@ -9,6 +16,7 @@ const todoReducer = (state = [], action) => {
       completed: false,
       id: v4()
     }
+    addToLocalStorage(newTodo)
     return [...state, newTodo]
   case TOGGLE_TODO:
     return state.map(todo => (
